618ce0fb4fa02f0e32da82bce2f1effa2ea7f68a
khushi22p@gmail.com <khushi22p@gmail.com>
2022-07-15 12:56:49 +0530

    project

--- input_fields.py
+++ input_fields.py
@@ -10,13 +10,12 @@ e.insert(0, "Enter your name")  # sets the default value of the box
 
 def myClick():
     hello = "Hello " + e.get()
-    myLabel = Label(root, text=hello)
+    myLabel = Label(root, text=hello) 
     myLabel.pack()
 
 
 myButton = Button(root, text="Enter your name",
                   padx=5, pady=5, command=myClick)
 myButton.pack()
-
 
 root.mainloop()
---
+++ practice.py
@@ -0,0 +1,17 @@
+# creating an empty list
+lst = []
+
+# number of elements as input
+n = int(input())
+
+# iterating till the range
+for i in range(0, n):
+	ele = int(input())
+    
+	lst.append(ele) # adding the element
+print(lst)
+
+
+
+	
+
